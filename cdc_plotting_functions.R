d4dfe6659627447d9afff0bca6db8a3517a8df37
Carl Pearson <cap10@ufl.edu>
2014-11-14 12:03:53 -0500

    update pdf / cdf plots

--- cdc_plotting_functions.R
+++ cdc_plotting_functions.R
@@ -4,27 +4,17 @@
 
 invisible(library(ggplot2)); invisible(library(reshape2))
 
-plotPeriodCDF <- function(distro, fillCol="lightsteelblue1") with(distro, {
-  par(bty="L",lwd=3)
-	par(mar=c(5,7,1,1)+1)
-	plot(NA, NA, bty="L", ann=F, xaxt="n", yaxt="n", xlim=c(0,max(days)), ylim=c(0,1))
-	axis(1, cex.axis=1.8, padj=.6, lwd=3)
-	mtext("Days post infection", 1, cex=1.5, line=3.5)
-	axis(2, cex.axis=1.8, padj=-.6, lwd=3)
-	mtext("Cumulative probability", 2, cex=1.5, line=6)
-	mtext("Normalized daily probability",2,cex=1.5,line=4)	
-	barplot(pdf/max(pdf),add=T,border=F,col=fillCol,space=0,width=1,ann=F,xaxt="n",yaxt="n")
-	# polygon(c(days,rev(days)),c(pdf/max(pdf),rep(0,length(days))),border=fillCol,col = fillCol)
-	points(c(days, length(days)), cdf, pch=19, type="l", lwd=6)
-	legend("right",
-    legend=c(
-      "Cumulative probability",
-      "Normalized daily probability"
-    ),
-    bty="n", cex=1.3, lwd=c(6, NA), lty=c(1, NA), pch=c(NA, 22),
-    col=c("black", fillCol), pt.bg=c(NA, fillCol), pt.cex=1.5
-  )
-})
+plotPeriodCDF <- function(distro) {
+  distro$pdf <- c(0, distro$pdf)
+  distro$days <- c(distro$days, length(distro$days))
+  distro.df <- data.frame(distro)
+  p <- ggplot(distro.df, aes(x=days, color=pdf, fill=pdf))
+  p <- p + geom_bar(aes(y=pdf/max(pdf)), stat="identity", width=0.95)
+  p <- p + geom_line(aes(y=cdf), color="black")
+  p <- p + xlab("Day") + ylab("CDF")
+  p <- p + theme_bw() + theme_update(panel.background = element_blank(), panel.border=element_blank())
+  p
+}
 
 dateSequence <- function(yyyymmdd, days) {
   ref.lt <- as.POSIXlt(yyyymmdd, "GMT")
@@ -138,24 +128,6 @@ plotBedOcc <- function(
   }
   
 }
-
-# plotBedOcc <- function(run,plotMeltzerCorrection=T,mcf=2.5,
-# 											 beginDate=as.Date("2014-03-26"),endDate=as.Date("2014-09-26"),startCDC=as.Date("2014-02-03"),
-# 											 lineCol="#2b8cbe",mortCol="red4"){
-# 	dates <- startCDC:(startCDC+dim(run)[1]-1)
-# 	par(bty="L",lwd=3,mar=c(5,7,1,1)+1)
-# 	if(plotMeltzerCorrection){
-# 		plot(dates, run[,3]/100*mcf, type="l",ann=F,xaxt="n",yaxt="n",col=lineCol,xlim=c(beginDate,endDate),lty=3)
-# 		lines(dates, run[,3]/100,col=lineCol)
-# 	}else{
-# 			plot(dates, run[,3]/100, type="l",ann=F,xaxt="n",yaxt="n",col=lineCol,xlim=c(beginDate,endDate))
-# 		}
-# 	mtext("Daily no. of beds in use",2,cex=1.7,line=6)
-# 	mtext("(Hundreds)",2,cex=1.7,line=4.3)
-# 	labDates <- as.Date(seq(beginDate,endDate,30))
-# 	axis(1,labDates,format(labDates,"%m/%e"),cex.axis=1.8,padj=.6,lwd=3)
-# 	axis(2,cex.axis=1.8,padj=-.6,lwd=3)
-# }
 
 plotInterventionScenario <- function(intervention.ref, simulation_duration,
     names=c("Hospitalization","Isolating Home Care","Non-isolating Care"),
